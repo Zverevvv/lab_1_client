fc6ad85d2c05bad5a097771d1a3c4102bec97f41
Vasiliy <Zv-v-v@mail.ru>
2014-09-26 08:08:48 +0400

    test2

---
+++ src/sample/t43gg.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Василий on 26.09.2014.
+ */
+public class t43gg {
+    private static final ObservableList<Book> data = FXCollections.observableArrayList();
+    private static ArrayList<IClient> clients = new ArrayList<>();
+}
