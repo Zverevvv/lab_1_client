89b9d1bd738647d37dca6673d5bdc86690084d37
Vasiliy <Zv-v-v@mail.ru>
2014-09-29 09:56:45 +0400

    rgreg

--- src/sample/t43gg.java
+++ src/sample/t43gg.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
  * Created by Василий on 26.09.2014.
  */
 public class t43gg {
-    private static final ObservableList<Book> data = FXCollections.observableArrayList();
-    private static ArrayList<IClient> clients = new ArrayList<>();
+    private static final ObservableList<Book>
 }
+data = FXCollections.observableArrayList();
+    private static ArrayList<IClient> clients = new ArrayList<>();
